e759f1f8db774b28f4f8584c2a9840d737f822ea
malthe-kea <mati0002@stud.kea.dk>
2024-02-28 13:37:44 +0100

    Tilføjet unit-test i MovieCollection.java for addMovie og getFilmList som begge passer.

--- Test/MovieCollectionTest.java
+++ Test/MovieCollectionTest.java
@@ -1,11 +1,39 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieCollectionTest {
 
     @org.junit.jupiter.api.Test
-    void getFilmListe() {
+    void getFilmListeWithZeroMovies() {
+
+        //Arrange
+        MovieCollection movieCollection = new MovieCollection();
+
+        //Act
+        int expectedResults = 0;
+        int actualResults = movieCollection.getMovieCollectionList().size();
+
+        //Assert
+        Assertions.assertEquals(expectedResults, actualResults);
+    }   @org.junit.jupiter.api.Test
+    void getFilmListeWithOneMovie() {
+
+        //Arrange
+        Movie movie = new Movie("Die Hard", "Malthe Bay", 1900,true, 190, "Horror", "1");
+        MovieCollection movieCollection = new MovieCollection();
+        movieCollection.addMovie(movie);
+
+        //Act
+        int expectedResults = 1;
+        int actualResults = movieCollection.getMovieCollectionList().size();
+
+        //Assert
+        Assertions.assertEquals(expectedResults, actualResults);
+    }
+    @org.junit.jupiter.api.Test
+    void getFilmListeWithThreeMovies() {
 
         //Arrange
         Movie movie = new Movie("Die Hard", "Malthe Bay", 1900,true, 190, "Horror", "1");
@@ -16,6 +44,7 @@ class MovieCollectionTest {
         movieCollection.addMovie(movie1);
         movieCollection.addMovie(movie2);
 
+
         //Act
         int expectedResults = 3;
         int actualResults = movieCollection.getMovieCollectionList().size();
@@ -25,20 +54,46 @@ class MovieCollectionTest {
     }
 
     @org.junit.jupiter.api.Test
-    void addMovie() {
-
+    void addOneMovie() {
         //Arrange
         Movie movie = new Movie("Die Hard", "Malthe Bay", 1900,true, 190, "Horror", "1");
-        Movie movie1 = new Movie("Die Hard 2 ", "Malthe Jensen ", 2010,false, 120, "Action", "2");
         MovieCollection movieCollection = new MovieCollection();
         movieCollection.addMovie(movie);
-        movieCollection.addMovie(movie1);
+        //Act
+        int expectedResult = 1;
+        int actualResult = movieCollection.getMovieCollectionList().size();
+        //Assert
+        Assertions.assertEquals(expectedResult,actualResult);
+
+    }
+    @org.junit.jupiter.api.Test
+    void addMoreMovies(){
+        //Arrange
+        Movie movie = new Movie("Die Hard", "Malthe Bay", 1900,true, 190, "Horror", "1");
+        Movie movie2 = new Movie("Harry Potter", "Kirk Hammet", 1992,false, 145, "Action", "2");
+
+        MovieCollection movieCollection = new MovieCollection();
+        movieCollection.addMovie(movie);
+        movieCollection.addMovie(movie2);
         //Act
         int expectedResult = 2;
         int actualResult = movieCollection.getMovieCollectionList().size();
         //Assert
         Assertions.assertEquals(expectedResult,actualResult);
+    }
 
+    @org.junit.jupiter.api.Test
+    void addZeroMovies(){
+        //Arrange
+        //Movie movie = new Movie("Die Hard", "Malthe Bay", 1900,true, 190, "Horror", "1");
+
+        MovieCollection movieCollection = new MovieCollection();
+        //movieCollection.addMovie(movie);
+        //Act
+        int expectedResult = 0;
+        int actualResult = movieCollection.getMovieCollectionList().size();
+        //Assert
+        Assertions.assertEquals(expectedResult,actualResult);
     }
 
     @org.junit.jupiter.api.Test
